8aac8bc7cdf0f1b8551785f5864119d61403ea94
Mikhail Matveev <xtreme@rh1.ru>
2023-01-04 15:23:16 +0200

    Bugfixes

--- dist/inject.js
+++ dist/inject.js
@@ -2,19 +2,21 @@ const fs = require("fs");
 const path = require("path");
 const zlib = require("zlib");
 
-if (!process.argv || !process.argv.length < 4) {
+if (!process.argv || process.argv.length < 3) {
     // eslint-disable-next-line no-console
-    console.log(`Create self-extracting JavaScript file\nUsage: sxjs <source.js> <target.js>`);
+    console.log(`Create self-extracting JavaScript file\nUsage: sfxjs <source.js> <target.js>`);
     process.exit(1);
 }
 try {
-    const srcFilePath = process.argv.length[2];
-    const destFilePath = process.argv.length[3];
+    const srcFilePath = process.argv[2];
+    const destFilePath = process.argv[3];
     const script = fs.readFileSync(path.resolve(__dirname, "loader.js"));
     const data = fs.readFileSync(srcFilePath);
     const compressed = Buffer.from(`//${zlib.brotliCompressSync(data).toString("hex")}`);
+    // eslint-disable-next-line no-console
     fs.writeFileSync(destFilePath, Buffer.concat([script, compressed]));
+    console.log(`All done, compressed file written to: ${destFilePath}`);
 } catch (e) {
     // eslint-disable-next-line no-console
-    console.log(e.message);
+    console.log(`Error: ${e.message}`);
 }
